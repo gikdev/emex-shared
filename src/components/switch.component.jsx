f40986be58fdd19be46e712f491939fbb7791061
gikdev <m.m.bhrm85@gmail.com>
2024-08-05 15:12:43 +0330

    Refactored `<Switch />` to make it simpler

--- src/components/switch.component.jsx
+++ src/components/switch.component.jsx
@@ -3,17 +3,16 @@ import { forwardRef } from "react"
 import { cn } from "../helpers"
 
 function Switch({ checked, ...delegated }, ref) {
-  const styles = {
-    root: cn(
-      "w-12 transition-all h-7 flex p-1 items-center rounded",
-      checked ? "bg-amberdark-9" : "bg-slatedark-6",
-      checked ? "justify-end" : "justify-start",
-    ),
-  }
+  const rootStyles = cn(
+    "w-12 transition-all h-7 flex p-1 items-center rounded",
+    checked ? "bg-amberdark-9" : "bg-slatedark-6",
+    checked ? "justify-end" : "justify-start",
+  )
+  const thumbStyles = cn("size-5 bg-slatedark-12 inline-block rounded shadow-lg")
 
   return (
-    <RadixSwitch.Root className={styles.root} ref={ref} {...delegated}>
-      <RadixSwitch.Thumb className="size-5 bg-slatedark-12 inline-block rounded shadow-lg" />
+    <RadixSwitch.Root checked={checked} className={rootStyles} {...delegated} ref={ref}>
+      <RadixSwitch.Thumb className={thumbStyles} />
     </RadixSwitch.Root>
   )
 }

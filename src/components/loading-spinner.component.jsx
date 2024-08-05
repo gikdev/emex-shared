b2c0399df00ace3fd5f3402542eec24498e7c1ab
gikdev <m.m.bhrm85@gmail.com>
2024-08-05 15:38:17 +0330

    Added a loading spinner component!

---
+++ src/components/loading-spinner.component.jsx
@@ -0,0 +1,11 @@
+import { CircleNotch } from "@phosphor-icons/react"
+
+function LoadingSpinner() {
+  return (
+    <span className="text-inherit">
+      <CircleNotch className="animate-spin" />
+    </span>
+  )
+}
+
+export default LoadingSpinner

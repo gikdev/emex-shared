e83ea38c325d8d4dc395ccbd4cdcf7d2df013578
gikdev <m.m.bhrm85@gmail.com>
2024-08-08 11:42:57 +0330

    Enhanced `useModal` to be more powerful 💪🏻

--- src/hooks/use-modal.hook.jsx
+++ src/hooks/use-modal.hook.jsx
@@ -1,8 +1,8 @@
 import { forwardRef, useCallback, useRef } from "react"
 
-const Modal = forwardRef(({ children }, ref) => (
-  <dialog onCancel={e => e.preventDefault()} className="modal" ref={ref}>
-    <div className="modal-box">{children}</div>
+const Modal = forwardRef(({ children, dialogProps, dialogContentContainerProps }, ref) => (
+  <dialog onCancel={e => e.preventDefault()} {...dialogProps} ref={ref}>
+    <div {...dialogContentContainerProps}>{children}</div>
   </dialog>
 ))
 
@@ -12,7 +12,15 @@ function useModal() {
   const closeModal = useCallback(() => dialogRef.current?.close(), [])
   const openModal = useCallback(() => dialogRef.current?.showModal(), [])
   const ModalComponent = useCallback(
-    ({ children }) => <Modal ref={dialogRef}>{children}</Modal>,
+    ({ children, dialogContentContainerProps, dialogProps }) => (
+      <Modal
+        dialogProps={dialogProps}
+        dialogContentContainerProps={dialogContentContainerProps}
+        ref={dialogRef}
+      >
+        {children}
+      </Modal>
+    ),
     [],
   )
 
